818f174677d321ce71b83db02e315068823be92a
tuna2134 <masato@priv.tuna2134.dev>
2024-08-30 11:26:26 +0000

    fix

--- src/server/result.rs
+++ src/server/result.rs
@@ -5,29 +5,6 @@ use axum::{
 };
 use serde::{Deserialize, Serialize};
 
-pub type AppResult<T> = Result<T, AppError>;
-
-pub struct AppError(anyhow::Error);
-
-impl IntoResponse for AppError {
-    fn into_response(self) -> Response {
-        (
-            StatusCode::INTERNAL_SERVER_ERROR,
-            format!("Something went wrong: {}", self.0),
-        )
-            .into_response()
-    }
-}
-
-impl<E> From<E> for AppError
-where
-    E: Into<anyhow::Error>,
-{
-    fn from(err: E) -> Self {
-        Self(err.into())
-    }
-}
-
 #[derive(Debug, Serialize, Deserialize)]
 pub struct ResponseAPIError {
     pub status: u16,
